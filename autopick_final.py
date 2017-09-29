e97baa12d5657f806296f33768ce2ad989ae387e
amlaanb <amlaanb@gmail.com>
2017-09-29 12:47:57 -0500

    Added final .py

---
+++ autopick_final.py
@@ -0,0 +1,146 @@
+# import libraries
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans, MiniBatchKMeans, DBSCAN
+from sklearn.metrics import silhouette_score
+import time
+import matplotlib.pyplot as plt
+from PIL import Image
+#from skimage import io, measure, color, filters, segmentation
+import cv2
+import argparse
+from skimage.transform import pyramid_gaussian
+from sklearn.preprocessing import scale
+import math
+#from sklearn.neighbors import DistanceMetric
+
+# silhouetteCoeff determination
+def silhouetteCoeff(z):
+	t0 = time.time()
+	max_silhouette = 0
+	max_k = 0
+	for i in range(6, 17):
+		clt = MiniBatchKMeans(n_clusters = i, random_state = 42)
+		clt.fit(z)
+		silhouette_avg = silhouette_score(z, clt.labels_, sample_size = 500, random_state = 42)
+		print("k: ", i, " silhouette avg: ", silhouette_avg)
+		if (silhouette_avg == 1.0):
+			max_k = i
+			print("Max k: ", max_k)
+			break
+		elif (silhouette_avg > max_silhouette):
+			max_silhouette = silhouette_avg
+			max_k = i
+	print("Max silhouette: ", max_silhouette)
+	print("Max k: ", max_k)
+	print("Time for silhouette: ", time.time() - t0)
+	return int(max_k)
+
+# manhattan distance function
+def dist(a, b):
+	return abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])
+
+# kMeans algorithm
+def kMeans(img):
+	t0 = time.time()
+	# apply kMeans, fit data, get histogram, get color bar
+	org_img = img
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	z = img.reshape((-1, 3))
+	#print(z.shape)
+
+	# downnsample images with gaussian smoothing
+	if (img.shape[0] > 250 or img.shape[1] > 250):
+		for (i, resized) in enumerate(pyramid_gaussian(org_img, downscale=2)):
+			if resized.shape[0] < 100 or resized.shape[1] < 100:
+				break
+			org_img = resized
+		#cv2.imshow("Layer {}".format(i + 1), resized)
+		#print(org_img.shape)
+
+	org_img = org_img.reshape((-1, 3))
+	org_img = scale(org_img)
+	#print(org_img.shape)
+	# kmeans
+	clt = MiniBatchKMeans(n_clusters = 16, random_state = 42)
+	clt.fit(z)
+	c_centers = clt.cluster_centers_
+
+	klt = MiniBatchKMeans(n_clusters = silhouetteCoeff(org_img), random_state = 42)
+	klt.fit(z)
+	k_centers = klt.cluster_centers_
+	
+	print("k_centers(b): ", k_centers)
+	print("c_centers(b): ", c_centers)
+	print("c_centers shape: ", c_centers.shape)
+	print("k_centers shape: ", k_centers.shape)
+
+
+	for (idx, i) in enumerate(k_centers):
+		d = 999999
+		max_jdx = 999
+		for (jdx, j) in enumerate(c_centers):
+			if dist(i, j) <= d:
+				d = dist(i, j)
+				#k_centers[idx, :] = c_centers[jdx, :]
+				max_jdx = jdx
+		k_centers[idx, :] = c_centers[max_jdx, :]
+		print(max_jdx)
+		c_centers = np.delete(c_centers, max_jdx, axis = 0)
+
+	print("c_centers(a): ", c_centers)
+	print("k_centers(a): ", k_centers)
+	hist = centroidHistogram(clt)
+	bar = plotColors(hist, clt.cluster_centers_, False)
+	print("Time including KMeans: ", time.time() - t0)
+	#print("unique labels: ", np.unique(np.array(clt.labels_), axis=0))
+
+	hist2 = centroidHistogram(klt)
+	bar2 = plotColors(hist2, klt.cluster_centers_, True)
+
+	plt.figure(1)
+	plt.axis("off")
+	plt.subplot(1, 2, 1)
+	plt.imshow(img)
+	plt.subplot(2, 2, 2)
+	plt.imshow(bar)
+	plt.subplot(2, 2, 4)
+	plt.imshow(bar2)
+	plt.show()
+
+def centroidHistogram(clt):
+	numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+	(hist, _) = np.histogram(clt.labels_, bins = numLabels)
+	hist = hist.astype("float")
+	hist /= hist.sum()
+	hist = np.sort(hist)[::-1]
+	print(hist)
+	return hist
+
+def plotColors(hist, centroids, stat):
+	if stat == False:
+		bar = np.zeros((50, 300, 3), dtype = "uint8")
+		startX = 0
+		for (percent, color) in zip(hist, centroids):
+			endX = startX + (percent * 300)
+			cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+				color.astype("uint8").tolist(), -1)
+			startX = endX
+		return bar
+	else:
+		bar = np.zeros((50, 300, 3), dtype = "uint8")
+		startX = 0
+		for (percent, color) in zip(hist, centroids):
+			endX = startX + (0.125 * 300)
+			cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+				color.astype("uint8").tolist(), -1)
+			startX = endX
+		return bar
+
+# read image
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to image")
+args = vars(ap.parse_args())
+
+img = cv2.imread(args["image"])
+kMeans(img)
